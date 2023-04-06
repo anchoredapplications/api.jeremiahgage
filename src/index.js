021958784ea45f4abaf5f45ee2429765a6966a11
Jeremiah Gage <jgage6@oreillyauto.com>
2023-04-06 11:26:29 -0500

    Build

--- src/index.js
+++ src/index.js
@@ -11,6 +11,10 @@ app.options('*', cors());
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json());
 
+app.get('/', async (req, res) => {
+    return res.send({ error: false, v: 1,});
+});
+
 routing.GET.forEach(({route, handler}) => {
     app.get(route, handler);
 });
