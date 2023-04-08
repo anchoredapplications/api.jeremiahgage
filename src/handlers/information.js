52ae2a9f50d4e960914586f407a6ee7a758fb7ec
Jeremiah Gage <jgage6@oreillyauto.com>
2023-04-07 23:40:27 -0500

    build #11 adding version

--- src/handlers/information.js
+++ src/handlers/information.js
@@ -3,7 +3,7 @@ require('dotenv').config()
 
 function Information(request, response) {    
     response.status(200);
-    response.json({ date: new Date(), secrets_populated: process.env.SECRETS_POPULATED})
+    response.json({ version_number:process.env.VERSION_NUMBER, secrets_populated: process.env.SECRETS_POPULATED, date: new Date()})
 }
 
 module.exports = { Information };
