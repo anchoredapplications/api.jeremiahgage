5a0a6aa8bdd5e052268f3577482f254535fd986e
Jeremiah Gage <jgage6@oreillyauto.com>
2023-04-07 23:47:53 -0500

    build #12 adding name

--- src/handlers/information.js
+++ src/handlers/information.js
@@ -3,7 +3,7 @@ require('dotenv').config()
 
 function Information(request, response) {    
     response.status(200);
-    response.json({ version_number:process.env.VERSION_NUMBER, secrets_populated: process.env.SECRETS_POPULATED, date: new Date()})
+    response.json({ author:"Jeremiah Gage", version_number:process.env.VERSION_NUMBER, secrets_populated: process.env.SECRETS_POPULATED, date: new Date()})
 }
 
 module.exports = { Information };
